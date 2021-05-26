8cc4cf395c5c3ae229ffc25421c78d165e2b50c6
AhmedSabry10696 <ahmed.sabry10696@gmail.com>
2021-05-26 13:00:53 +0200

    dc motor local config file

---
+++ HAL/DC_Motor_Lcfg.h
@@ -0,0 +1,23 @@
+/**
+ * @file DC_Motor_Lcfg.h
+ * @author Ahmed Sabry (ahmed.sabry10696@gmail.com)
+ * @brief DC Motor Local configuration file
+ * @version 0.1
+ * @date 26-05-2021
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef DC_MOTOR_LCFG_H_
+#define DC_MOTOR_LCFG_H_
+
+#include "DIO_Interface.h"
+
+/**** Motor PINS ****/
+#define EN  PINB0
+#define IN1 PINB1
+#define IN2 PINB2 
+/*******************/
+
+#endif /* DC_MOTOR_LCFG_H_ */
