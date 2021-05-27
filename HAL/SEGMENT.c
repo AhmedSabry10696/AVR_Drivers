568896392336ad6a45b29451edfc3a5ea69bad86
AhmedSabry10696 <ahmed.sabry10696@gmail.com>
2021-05-27 15:15:01 +0200

    segment driver iimplementaion

--- HAL/SEGMENT.c
+++ HAL/SEGMENT.c
@@ -16,20 +16,26 @@
 	static u8 Seg_Nums[] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6f, 0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71}; 
 #endif
 
-void SEGMENT_Display_Digit(u8 num)
+void SEGMENT_Display(u8 num)
 {
 	#ifndef	BCD
 		#if SEGMENT_TYPE == COM_CATHODE
-			DIO_WritePort(SEGMENT_PORT,Seg_Nums[num]);
+			DIO_WritePort(SEGMENT_PORT,Seg_Nums[num%16]);
 			
 		#elif SEGMENT_TYPE == COM_ANODE
-			DIO_WritePort(SEGMENT_PORT,~(Seg_Nums[num]));
+			DIO_WritePort(SEGMENT_PORT,~(Seg_Nums[num%16]));
 		#endif
 	#else
 		#if BCD == LOW_PINS
 			DIO_EditPort_LowerHalf(SEGMENT_PORT,num);
 		#elif BCD == HIGH_PINS
 			DIO_EditPort_HigherHalf(SEGMENT_PORT,num);
+		#elif BCD == FULL_PORT
+			u8 digit = num%10;
+			DIO_EditPort_LowerHalf(SEGMENT_PORT,digit);
+			num /= 10;
+			digit = num%10;
+			DIO_EditPort_HigherHalf(SEGMENT_PORT,digit);
 		#endif
 	#endif
 }
