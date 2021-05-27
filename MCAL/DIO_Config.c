7972a269d26739643c5df11bc60b527b8c67cdfb
AhmedSabry10696 <ahmed.sabry10696@gmail.com>
2021-05-27 03:42:07 +0200

    DIO config file

---
+++ MCAL/DIO_Config.c
@@ -0,0 +1,55 @@
+/**
+ * @file DIO_Config.c
+ * @author Ahmed Sabry (ahmed.sabry10696@gmail.com)
+ * @brief DIO configuration file
+ * @version 0.1
+ * @date 25-05-2021
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include "DIO_Interface.h"
+
+const DIO_PinStatus_type PinsStatus[TOTAL_PINS] = 
+{
+    /* PORTA PINS 0:7*/
+    OUTPUT,
+    OUTPUT,
+    OUTPUT,
+    OUTPUT,
+    OUTPUT,
+    OUTPUT,
+    OUTPUT,
+    OUTPUT,
+
+    /* PORTB PINS 0:7*/
+    OUTPUT,
+    OUTPUT,
+    OUTPUT,
+    OUTPUT,
+    OUTPUT,
+    OUTPUT,
+    OUTPUT,
+    OUTPUT,
+
+    /* PORTC PINS 0:7*/    
+    OUTPUT,
+    OUTPUT,
+    OUTPUT,
+    OUTPUT,
+    OUTPUT,
+    OUTPUT,
+    OUTPUT,
+    OUTPUT,
+
+    /* PORTD PINS 0:7*/
+    OUTPUT,
+    OUTPUT,
+    OUTPUT,
+    OUTPUT,
+    OUTPUT,
+    OUTPUT,
+    OUTPUT,
+    OUTPUT
+};
