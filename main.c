1d35665839f69d461f2650eb6c251bfa0ade6ccb
AhmedSabry10696 <ahmed.sabry10696@gmail.com>
2021-05-27 03:39:18 +0200

    application

--- main.c
+++ main.c
@@ -3,30 +3,19 @@
 #include <util/delay.h>
 
 #include "DIO_Interface.h"
-#include "DC_Motor.h"
+#include "Segment.h"
 
 int main(void)
 {
 	DIO_Init();  
-	DIO_WritePin(PINB0,HIGH);
-	DIO_WritePin(PINB1,HIGH);
-	_delay_ms(3000);
+	
     while (1) 
     {
-		if (DIO_ReadPin(PINC0) == LOW)
+		for (char i = 0; i < 16; i++)
 		{
-			DC_Motor_CW();
-			
+			SEGMENT_Display_Digit(i);
+			_delay_ms(1000);
 		}
-		if (DIO_ReadPin(PINC1) == LOW)
-		{
-			DC_Motor_CCW();
-		}
-		if(DIO_ReadPin(PINC2) == LOW)
-		{
-			DC_Motor_Stop();
-		}
-		_delay_ms(200);
     }
 }
 
