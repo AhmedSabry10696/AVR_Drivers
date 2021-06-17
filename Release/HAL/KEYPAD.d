eac3b58e9faf12435f64cfe76b67af7190d98133
AhmedSabry10696 <ahmed.sabry10696@gmail.com>
2021-06-18 00:21:32 +0200

    Atmel studio building process

---
+++ Release/HAL/KEYPAD.d
@@ -0,0 +1,15 @@
+HAL/KEYPAD.d HAL/KEYPAD.o: ../HAL/KEYPAD.c ../HAL/Keypad.h \
+ ../MCAL/DIO_Interface.h ../LIB/StdTypes.h ../LIB/MemMap.h \
+ ../LIB/StdTypes.h ../LIB/Utils.h
+
+../HAL/Keypad.h:
+
+../MCAL/DIO_Interface.h:
+
+../LIB/StdTypes.h:
+
+../LIB/MemMap.h:
+
+../LIB/StdTypes.h:
+
+../LIB/Utils.h:
--- Release/HAL/Segment.d
+++ Release/HAL/Segment.d
@@ -1,4 +1,4 @@
-HAL/Segment.d HAL/Segment.o: ../HAL/Segment.c ../HAL/SEGMENT_Interface.h \
+HAL/SEGMENT.d HAL/SEGMENT.o: ../HAL/SEGMENT.c ../HAL/SEGMENT_Interface.h \
  ../LIB/StdTypes.h ../MCAL/DIO_Interface.h ../LIB/MemMap.h \
  ../LIB/StdTypes.h ../LIB/Utils.h ../HAL/SEGMENT_Config.h
 
