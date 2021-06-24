ee765b0fd485f3961c978339da521b9b44e54bd7
AhmedSabry10696 <ahmed.sabry10696@gmail.com>
2021-06-24 11:24:14 +0200

    Memory Map file

--- LIB/MemMap.h
+++ LIB/MemMap.h
@@ -58,4 +58,24 @@
 #define ADC     (*((volatile u16*)0x24))
 /****************************************/
 
+/********External Interrupt REG*********/
+#define MCUCR   (*((volatile u8 *)0x55))
+#define ISC00   0
+#define ISC01   1
+#define ISC10   2
+#define ISC11   3
+
+#define MCUCSR  (*((volatile u8 *)0x54))
+#define ISC2    6
+
+#define GICR    (*((volatile u8 *)0x5B))
+#define INT2    5
+#define INT0    6
+#define INT1    7
+
+#define GIFR    (*((volatile u8 *)0x5A))
+#define INTF2   5
+#define INTF0   6
+#define INTF1   7
+/***************************************/
 #endif /* MEMMAP_H_ */
