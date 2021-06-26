33a13980ffb8c608dd87a857c7474d4d428844f6
AhmedSabry10696 <ahmed.sabry10696@gmail.com>
2021-06-26 20:48:35 +0200

    stepper motor driver

---
+++ HAL/Stepper_Motor.c
@@ -0,0 +1,222 @@
+/**
+ * @file Stepper_Motor.c
+ * @author Ahmed Sabry (ahmed.sabry10696@gmail.com)
+ * @brief Stepper motor driver
+ * @version 0.1
+ * @date 26-06-2021
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include "Stepper_Motor.h"
+
+void Stepper_BiPolarFull_CW(void)
+{
+    DIO_WritePin(COIL1A,HIGH);
+    DIO_WritePin(COIL1B,LOW);
+    DIO_WritePin(COIL2A,LOW);
+    DIO_WritePin(COIL2B,LOW);
+    _delay_ms(DELAY);
+
+    DIO_WritePin(COIL1A,LOW);
+    DIO_WritePin(COIL1B,LOW);
+    DIO_WritePin(COIL2A,HIGH);
+    DIO_WritePin(COIL2B,LOW);
+    _delay_ms(DELAY);
+
+    DIO_WritePin(COIL1A,LOW);
+    DIO_WritePin(COIL1B,HIGH);
+    DIO_WritePin(COIL2A,LOW);
+    DIO_WritePin(COIL2B,LOW);
+    _delay_ms(DELAY);
+
+    DIO_WritePin(COIL1A,LOW);
+    DIO_WritePin(COIL1B,LOW);
+    DIO_WritePin(COIL2A,LOW);
+    DIO_WritePin(COIL2B,HIGH);
+    _delay_ms(DELAY);
+}
+
+void Stepper_BiPolarFull_CCW(void)
+{
+    DIO_WritePin(COIL1A,LOW);
+    DIO_WritePin(COIL1B,LOW);
+    DIO_WritePin(COIL2A,LOW);
+    DIO_WritePin(COIL2B,HIGH);
+    _delay_ms(DELAY);
+
+    DIO_WritePin(COIL1A,LOW);
+    DIO_WritePin(COIL1B,HIGH);
+    DIO_WritePin(COIL2A,LOW);
+    DIO_WritePin(COIL2B,LOW);
+    _delay_ms(DELAY);
+
+    DIO_WritePin(COIL1A,LOW);
+    DIO_WritePin(COIL1B,LOW);
+    DIO_WritePin(COIL2A,HIGH);
+    DIO_WritePin(COIL2B,LOW);
+    _delay_ms(DELAY);
+
+    DIO_WritePin(COIL1A,HIGH);
+    DIO_WritePin(COIL1B,LOW);
+    DIO_WritePin(COIL2A,LOW);
+    DIO_WritePin(COIL2B,LOW);
+    _delay_ms(DELAY);
+}
+
+void Stepper_UniPolarFull_CW(void)
+{   
+    DIO_WritePin(COIL1A,HIGH);
+    DIO_WritePin(COIL1B,LOW);
+    DIO_WritePin(COIL2A,LOW);
+    DIO_WritePin(COIL2B,LOW);
+    _delay_ms(DELAY);
+
+    DIO_WritePin(COIL1A,LOW);
+    DIO_WritePin(COIL1B,HIGH);
+    DIO_WritePin(COIL2A,LOW);
+    DIO_WritePin(COIL2B,LOW);
+    _delay_ms(DELAY);
+
+    DIO_WritePin(COIL1A,LOW);
+    DIO_WritePin(COIL1B,LOW);
+    DIO_WritePin(COIL2A,HIGH);
+    DIO_WritePin(COIL2B,LOW);
+    _delay_ms(DELAY);
+
+    DIO_WritePin(COIL1A,LOW);
+    DIO_WritePin(COIL1B,LOW);
+    DIO_WritePin(COIL2A,LOW);
+    DIO_WritePin(COIL2B,HIGH);
+    _delay_ms(DELAY);
+}
+
+void Stepper_UniPolarFull_CCW(void)
+{
+    DIO_WritePin(COIL1A,LOW);
+    DIO_WritePin(COIL1B,LOW);
+    DIO_WritePin(COIL2A,LOW);
+    DIO_WritePin(COIL2B,HIGH);
+    _delay_ms(DELAY);
+
+    DIO_WritePin(COIL1A,LOW);
+    DIO_WritePin(COIL1B,LOW);
+    DIO_WritePin(COIL2A,HIGH);
+    DIO_WritePin(COIL2B,LOW);
+    _delay_ms(DELAY);
+
+    DIO_WritePin(COIL1A,LOW);
+    DIO_WritePin(COIL1B,HIGH);
+    DIO_WritePin(COIL2A,LOW);
+    DIO_WritePin(COIL2B,LOW);
+    _delay_ms(DELAY);
+
+    DIO_WritePin(COIL1A,HIGH);
+    DIO_WritePin(COIL1B,LOW);
+    DIO_WritePin(COIL2A,LOW);
+    DIO_WritePin(COIL2B,LOW);
+    _delay_ms(DELAY);
+}
+
+void Stepper_UniPolarHalf_CW(void)
+{
+    DIO_WritePin(COIL1A,HIGH);
+    DIO_WritePin(COIL1B,LOW);
+    DIO_WritePin(COIL2A,LOW);
+    DIO_WritePin(COIL2B,LOW);
+    _delay_ms(DELAY);
+
+    DIO_WritePin(COIL1A,HIGH);
+    DIO_WritePin(COIL1B,HIGH);
+    DIO_WritePin(COIL2A,LOW);
+    DIO_WritePin(COIL2B,LOW);
+    _delay_ms(DELAY);
+
+    DIO_WritePin(COIL1A,LOW);
+    DIO_WritePin(COIL1B,HIGH);
+    DIO_WritePin(COIL2A,LOW);
+    DIO_WritePin(COIL2B,LOW);
+    _delay_ms(DELAY);
+
+    DIO_WritePin(COIL1A,LOW);
+    DIO_WritePin(COIL1B,HIGH);
+    DIO_WritePin(COIL2A,HIGH);
+    DIO_WritePin(COIL2B,LOW);
+    _delay_ms(DELAY);
+
+    DIO_WritePin(COIL1A,LOW);
+    DIO_WritePin(COIL1B,LOW);
+    DIO_WritePin(COIL2A,HIGH);
+    DIO_WritePin(COIL2B,LOW);
+    _delay_ms(DELAY);
+
+    DIO_WritePin(COIL1A,LOW);
+    DIO_WritePin(COIL1B,LOW);
+    DIO_WritePin(COIL2A,HIGH);
+    DIO_WritePin(COIL2B,HIGH);
+    _delay_ms(DELAY);
+
+    DIO_WritePin(COIL1A,LOW);
+    DIO_WritePin(COIL1B,LOW);
+    DIO_WritePin(COIL2A,LOW);
+    DIO_WritePin(COIL2B,HIGH);
+    _delay_ms(DELAY);
+
+    DIO_WritePin(COIL1A,HIGH);
+    DIO_WritePin(COIL1B,LOW);
+    DIO_WritePin(COIL2A,LOW);
+    DIO_WritePin(COIL2B,HIGH);
+    _delay_ms(DELAY);
+}
+
+void Stepper_UniPolarHalf_CCW(void)
+{
+    DIO_WritePin(COIL1A,HIGH);
+    DIO_WritePin(COIL1B,LOW);
+    DIO_WritePin(COIL2A,LOW);
+    DIO_WritePin(COIL2B,HIGH);
+    _delay_ms(DELAY);
+
+    DIO_WritePin(COIL1A,LOW);
+    DIO_WritePin(COIL1B,LOW);
+    DIO_WritePin(COIL2A,LOW);
+    DIO_WritePin(COIL2B,HIGH);
+    _delay_ms(DELAY);
+
+    DIO_WritePin(COIL1A,LOW);
+    DIO_WritePin(COIL1B,LOW);
+    DIO_WritePin(COIL2A,HIGH);
+    DIO_WritePin(COIL2B,HIGH);
+    _delay_ms(DELAY);
+
+    DIO_WritePin(COIL1A,LOW);
+    DIO_WritePin(COIL1B,LOW);
+    DIO_WritePin(COIL2A,HIGH);
+    DIO_WritePin(COIL2B,LOW);
+    _delay_ms(DELAY);
+
+    DIO_WritePin(COIL1A,LOW);
+    DIO_WritePin(COIL1B,HIGH);
+    DIO_WritePin(COIL2A,HIGH);
+    DIO_WritePin(COIL2B,LOW);
+    _delay_ms(DELAY);
+
+    DIO_WritePin(COIL1A,LOW);
+    DIO_WritePin(COIL1B,HIGH);
+    DIO_WritePin(COIL2A,LOW);
+    DIO_WritePin(COIL2B,LOW);
+    _delay_ms(DELAY);
+
+    DIO_WritePin(COIL1A,HIGH);
+    DIO_WritePin(COIL1B,HIGH);
+    DIO_WritePin(COIL2A,LOW);
+    DIO_WritePin(COIL2B,LOW);
+    _delay_ms(DELAY);
+
+    DIO_WritePin(COIL1A,HIGH);
+    DIO_WritePin(COIL1B,LOW);
+    DIO_WritePin(COIL2A,LOW);
+    DIO_WritePin(COIL2B,LOW);
+    _delay_ms(DELAY);
+}
---
+++ HAL/Stepper_Motor.h
@@ -0,0 +1,66 @@
+/**
+ * @file Stepper_Motor.h
+ * @author Ahmed Sabry (ahmed.sabry10696@gmail.com)
+ * @brief Stepper motor header file
+ * @version 0.1
+ * @date 26-06-2021
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef STEPPER_MOTOR_H_
+#define STEPPER_MOTOR_H_
+
+#define F_CPU 8000000UL
+#include "StdTypes.h"
+#include "DIO_Interface.h"
+#include <util/delay.h>
+
+#define DELAY 100
+
+/* stepper motor control pins */
+#define COIL1A  PINC0
+#define COIL1B  PINC1
+#define COIL2A  PINC2
+#define COIL2B  PINC3
+
+/**
+ * @brief rotate stepper 4 steps in clock wise direction
+ * @note  full stepping one phase on
+ */
+extern void Stepper_BiPolarFull_CW(void);
+
+/**
+ * @brief rotate stepper 4 steps in anti clock wise direction
+ * @note  full stepping one phase on
+ */
+extern void Stepper_BiPolarFull_CCW(void);
+
+/**
+ * @brief rotate stepper 4 steps in clock wise direction
+ * @note  full stepping one phase on
+ */
+extern void Stepper_UniPolarFull_CW(void);
+
+/**
+ * @brief rotate stepper 4 steps in anti clock wise direction
+ * @note  full stepping one phase on
+ */
+extern void Stepper_UniPolarFull_CCW(void);
+
+/**
+ * @brief rotate stepper 4 steps in clock wise direction
+ * @note  half stepping
+ */
+extern void Stepper_UniPolarHalf_CW(void);
+
+/**
+ * @brief rotate stepper 4 steps in anti clock wise direction
+ * @note  half stepping
+ */
+extern void Stepper_UniPolarHalf_CCW(void);
+
+
+#endif /* STEPPER_MOTOR_H_ */
+
