2205864460be79ec0d733adf385292eb7f4ecb23
AhmedSabry10696 <ahmed.sabry10696@gmail.com>
2021-06-27 18:46:15 +0200

    APP

--- main.c
+++ main.c
@@ -2,29 +2,53 @@
 #include <util/delay.h>
 #include "MemMap.h"
 #include "Utils.h"
-#include "Sensors.h"
 #include "DIO_Interface.h"
-#include "LCD_Interface.h"
-#include "ADC_Interface.h"
+#include "Ext_Interrupt.h"
+
+void toggle_led1(void)
+{
+	DIO_TogglePin(PINC0);
+	_delay_ms(2000);
+	DIO_TogglePin(PINC0);
+	_delay_ms(2000);
+}
+void toggle_led2(void)
+{
+	DIO_TogglePin(PINC1);
+	_delay_ms(2000);
+	DIO_TogglePin(PINC1);
+	_delay_ms(2000);
+}
+void toggle_led3(void)
+{
+	DIO_TogglePin(PINC2);
+	_delay_ms(2000);
+	DIO_TogglePin(PINC2);
+	_delay_ms(2000);
+}
 
 int main(void)
 {	
-	u16 x;
 	/* DIO init */
 	DIO_Init();
-	ADC_Init(REF_AREF,ADC_Scaler_64);
-	LCD_Init();
+	sei();
+	EXI_Enable(EX_INT0);
+	EXI_Enable(EX_INT1);
+	EXI_Enable(EX_INT2);
 	
-	LCD_WriteString("MPX4110 pres sensor");
-	
+	EXI_TriggerEdge(EX_INT0,FALLING_EDGE);
+	EXI_TriggerEdge(EX_INT1,FALLING_EDGE);
+	EXI_TriggerEdge(EX_INT0,FALLING_EDGE);
+
+	EXI_SetCallBack(EX_INT0,toggle_led1);
+	EXI_SetCallBack(EX_INT1,toggle_led2);
+	EXI_SetCallBack(EX_INT2,toggle_led3);
+
 	while(1)
 	{
-		LCD_GoTo(2,0);
-		x= MPX4110_PressureRead();
-		LCD_WriteNum(x/10);
-		LCD_WriteChar('.');
-		LCD_WriteNum(x%10);
-		LCD_WriteString("    KPa");
-		LCD_WriteString("   ");
+		DIO_TogglePin(PINC3);
+		_delay_ms(500);
+		DIO_TogglePin(PINC3);
+		_delay_ms(500);
 	}
 }
