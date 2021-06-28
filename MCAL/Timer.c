162a09ff5706f73663279b4753c3286b8e2a7e3d
AhmedSabry10696 <ahmed.sabry10696@gmail.com>
2021-06-28 23:50:33 +0200

    Timer Driver

---
+++ MCAL/Timer.c
@@ -0,0 +1,12 @@
+/**
+ * @file Timer.c
+ * @author Ahmed Sabry (ahmed.sabry10696@gmail.com)
+ * @brief Timer driver 
+ * @version 0.1
+ * @date 28-06-2021
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include "Timer.h"
---
+++ MCAL/Timer.h
@@ -0,0 +1,21 @@
+/**
+ * @file Timer.h
+ * @author Ahmed Sabry (ahmed.sabry10696@gmail.com)
+ * @brief Timer header file
+ * @version 0.1
+ * @date 28-06-2021
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef TIMER_H_
+#define TIMER_H_
+
+#include "StdTypes.h"
+#include "Utils.h"
+#include "MemMap.h"
+
+
+
+#endif /* TIMER_H_ */
