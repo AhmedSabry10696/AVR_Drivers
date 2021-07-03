5475207cba1b3844cfa3c2312cc6d9c5bea0d97e
AhmedSabry10696 <ahmed.sabry10696@gmail.com>
2021-07-03 20:03:00 +0200

    Memory map file

--- LIB/MemMap.h
+++ LIB/MemMap.h
@@ -77,6 +77,7 @@
 #define INTF1   7
 
 /**************** Interrupt service routine ********************/
+#define _VECTOR(N) __vector_ ## N
 
 /* ISR runs with global interrupts initially enabled */
 /* infinitely recursive interrupts: UART interrupts or level-triggered external interrupts */
@@ -85,17 +86,17 @@
 /* ISR is created with no prologue or epilogue code */
 #define ISR_NAKED     __attribute__((naked))
 
+/* ISR Macro with variable arguments */
 #define ISR(vector,...) void vector (void) __attribute__ ((signal))__VA_ARGS__;\
 void vector (void)
 
 /************ Interrupt functions ************/
 # define sei()  __asm__ __volatile__ ("sei" ::)
 # define cli()  __asm__ __volatile__ ("cli" ::)
-# define reti() __asm__ __volatile__ ("reti" ::)
 # define ret()  __asm__ __volatile__ ("ret" ::)
+# define reti() __asm__ __volatile__ ("reti" ::)
 
 /********** Interrupt Vector Table ***********/
-
 /* External Interrupt Request 0 */
 #define INT0_VECT __vector_1
 /* External Interrupt Request 1 */
@@ -138,7 +139,6 @@ void vector (void)
 #define SPM_RDY_VECT __vector_20
 /* Bad Interrupt Vector */
 #define BAD_VECT __vector_default
-/*******************************************/
 
 /******************** Timers REG *********************/
 #define TWCR    (*((volatile u8*)0x56))
@@ -182,9 +182,26 @@ void vector (void)
 #define TCNT1   (*((volatile u16*)0x4C))
 #define TCNT1L  (*((volatile u8*)0x4C))
 #define TCNT1H  (*((volatile u8*)0x4D))
+
 #define TCCR1B  (*((volatile u8*)0x4E))
+#define ICNC1   7
+#define ICES1   6
+#define WGM13   4
+#define WGM12   3
+#define CS12    2
+#define CS11    1
+#define CS10    0
+
 #define TCCR1A  (*((volatile u8*)0x4F))
-#define SFIOR   (*((volatile u8*)0x50))
+#define COM1A1  7
+#define COM1A0  6
+#define COM1B1  5
+#define COM1B0  4
+#define FOC1A   3
+#define FOC1B   2
+#define WGM11   1
+#define WGM10   0
+
 #define OSCCAL  (*((volatile u8*)0x51))
 
 /************* Timer 2 *****************/
@@ -201,39 +218,170 @@ void vector (void)
 #define CS21    1
 #define CS20    0
 
-/* ASSR */
-/* bits 7-4 reserved */
+/*
+//ASSR
 #define AS2     3
 #define TCN2UB  2
 #define OCR2UB  1
 #define TCR2UB  0
 
-/* TCCR1A */
-#define COM1A1  7
-#define COM1A0  6
-#define COM1B1  5
-#define COM1B0  4
-#define FOC1A   3
-#define FOC1B   2
-#define WGM11   1
-#define WGM10   0
-
-/* TCCR1B */
-#define ICNC1   7
-#define ICES1   6
-/* bit 5 reserved */
-#define WGM13   4
-#define WGM12   3
-#define CS12    2
-#define CS11    1
-#define CS10    0
-
-/* WDTCR */
 #define WDTCR    (*((volatile u8*)0x41))
 #define WDTOE   4
 #define WDE     3
 #define WDP2    2
 #define WDP1    1
 #define WDP0    0
+*/
+
+/*****************UART Registers*****************/
+/* USART Baud Rate Register Low */
+#define UBRRL   (*(volatile u8*)0x29)
+
+/* USART Control and Status Register B */
+#define UCSRB   (*(volatile u8*)0x2A)
+#define RXCIE   7
+#define TXCIE   6
+#define UDRIE   5
+#define RXEN    4
+#define TXEN    3
+#define UCSZ    2
+#define UCSZ2   2
+#define RXB8    1
+#define TXB8    0
+
+/* USART Control and Status Register A */
+#define UCSRA   (*(volatile u8*)0x2B)
+#define RXC     7
+#define TXC     6
+#define UDRE    5
+#define FE      4
+#define DOR     3
+#define UPE     2
+#define U2X     1
+#define MPCM    0
+
+/* USART I/O Data Register */
+#define UDR     (*(volatile u8*)0x2C)
+
+/* USART Baud Rate Register High */
+#define UBRRH   (*(volatile u8*)0x40)
+
+/* USART Control and Status Register C */
+#define UCSRC   (*(volatile u8*)0x40)
+
+/* USART Register C */
+#define URSEL        7
+#define UMSEL        6
+#define UPM1         5
+#define UPM0         4
+#define USBS         3
+#define UCSZ1        2
+#define UCSZ0        1
+#define UCPOL        0
+
+/************** SPI REG **************/
+/* SPI Control Register */
+#define SPCR    (*(volatile u8*)0x2D)
+#define SPIE         7
+#define SPE          6
+#define DORD         5
+#define MSTR         4
+#define CPOL         3
+#define CPHA         2
+#define SPR1         1
+#define SPR0         0
+
+/* SPI Status Register */
+#define SPSR    (*(volatile u8*)0x2E)
+#define SPIF         7
+#define WCOL         6
+#define SPI2X        0
+
+/* SPI I/O Data Register */
+#define SPDR    (*(volatile u8*)0x2F)
+
+
+/****************************** EEPROM Control Register ****************************/
+/* EEPROM Control Register */
+#define EECR	(*(volatile unsigned char*)0x3C)
+
+#define    EERIE        3
+#define    EEMWE        2
+#define    EEWE         1
+#define    EERE         0
+
+/* EEPROM Data Register */
+#define EEDR	(*(volatile unsigned char*)0x3D)
+
+/* EEPROM Address Register */
+#define EEAR	(*(volatile unsigned short*)0x3E)
+#define EEARL	(*(volatile unsigned char*)0x3E)
+#define EEARH	(*(volatile unsigned char*)0x3F)
+
+/* TWI stands for "Two Wire Interface" or "TWI Was I2C(tm)" */
+#define TWBR    (*(volatile unsigned char*)0x20)
+#define TWSR    (*(volatile unsigned char*)0x21)
+#define TWAR    (*(volatile unsigned char*)0x22)
+#define TWDR    (*(volatile unsigned char*)0x23)
+
+/* TIMSK */
+#define OCIE2   7
+#define TOIE2   6
+#define TICIE1  5
+#define OCIE1A  4
+#define OCIE1B  3
+#define TOIE1   2
+#define OCIE0   1
+#define TOIE0   0
+
+/* TIFR */
+#define OCF2    7
+#define TOV2    6
+#define ICF1    5
+#define OCF1A   4
+#define OCF1B   3
+#define TOV1    2
+#define OCF0    1
+#define TOV0    0
+
+/* SPMCR */
+#define SPMIE   7
+#define RWWSB   6
+/* bit 5 reserved */
+#define RWWSRE  4
+#define BLBSET  3
+#define PGWRT   2
+#define PGERS   1
+#define SPMEN   0
+
+/* TWCR */
+#define TWINT   7
+#define TWEA    6
+#define TWSTA   5
+#define TWSTO   4
+#define TWWC    3
+#define TWEN    2
+/* bit 1 reserved */
+#define TWIE    0
+
+/* TWAR */
+#define TWA6    7
+#define TWA5    6
+#define TWA4    5
+#define TWA3    4
+#define TWA2    3
+#define TWA1    2
+#define TWA0    1
+#define TWGCE   0
+
+/* TWSR */
+#define TWS7    7
+#define TWS6    6
+#define TWS5    5
+#define TWS4    4
+#define TWS3    3
+/* bit 2 reserved */
+#define TWPS1   1
+#define TWPS0   0
 
 #endif /* MEMMAP_H_ */
