fee9286797c2837fd2e760f047c8207e5d06bb23
AhmedSabry10696 <ahmed.sabry10696@gmail.com>
2021-07-04 17:39:14 +0200

    Servo motor driver

---
+++ HAL/Servo_Motor.h
@@ -0,0 +1,24 @@
+/**
+ * @file Servo_Motor.h
+ * @author Ahmed Sabry (ahmed.sabry10696@gmail.com)
+ * @brief Servo motor header file
+ * @version 0.1
+ * @date 04-07-2021
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include "Timer.h"
+
+/**
+ * @brief Servo motor initialization f= 50HZ
+ * 
+ */
+extern void Servo_Init(void);
+/**
+ * @brief Set angle of Servo motor
+ * 
+ * @param angle 0:180
+ */
+extern void Servo_SetPosition(u8 angle);
