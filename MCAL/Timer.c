c872a1e8aead01129da7faf024e5e4fc80fd397b
AhmedSabry10696 <ahmed.sabry10696@gmail.com>
2021-07-04 17:39:37 +0200

    Timers driver

--- MCAL/Timer.c
+++ MCAL/Timer.c
@@ -11,87 +11,272 @@
 
 #include "Timer.h"
 
-/* pointer to function for Timer overflow interrupt */
 static void (*TIMER0_OVF_IntFptr)(void) = NULLPTR;
-
-/* pointer to function for Timer compare match interrupt */
 static void (*TIMER0_COM_IntFptr)(void) = NULLPTR;
 
-void TIMER0_Init(TIMER0_Mode_Type mode,TIMER0_Scaler_Type scaler,TIMER0_OC0_Type oc0)
-{
-    switch (mode)
-    {
-        case TIMER0_Normal:
-            switch (oc0)
-            {
-                case OC0_Disconnected:
-                    TCCR0 = 0x80;
-                    break;
-                case OC0_Toggle_OnCompMatch:
-                    TCCR0 = 0x90;
-                    break;
-                case OC0_Clear_OnCompMatch:
-                    TCCR0 = 0xA0;
-                    break;
-                case OC0_Set_OnCompMatch:
-                    TCCR0 = 0xB0;
-                    break;
-            }
-            break;
-        case TIMER0_PWM_PHASE_CORRECT:
-            break;
-        case TIMER0_CTC:
-            switch (oc0)
-            {
-                case OC0_Disconnected:
-                    TCCR0 = 0x88;
-                    break;
-                case OC0_Toggle_OnCompMatch:
-                    TCCR0 = 0x98;
-                    break;
-                case OC0_Clear_OnCompMatch:
-                    TCCR0 = 0xA8;
-                    break;
-                case OC0_Set_OnCompMatch:
-                    TCCR0 = 0xB8;
-                    break;
-            }
-            break;
-        case TIMER0_FAST_PWM:
-            break;
-    }
+static void (*Timer1_OVF_IntFptr)(void) = NULLPTR;
+static void (*Timer1_OCA_IntFptr)(void) = NULLPTR;
+static void (*Timer1_OCB_IntFptr)(void) = NULLPTR;
+static void (*Timer1_ICU_IntFptr)(void) = NULLPTR;
 
+void Timer0_Init(TIMER0_Mode_type mode,TIMER0_Scaler_type scaler)
+{
+	switch (mode)
+	{
+		case TIMER0_NORMAL:
+            CLEAR_BIT(TCCR0,WGM00);
+            CLEAR_BIT(TCCR0,WGM01);
+            break;
+		case TIMER0_PWM_PHASE_CORRECT:
+            SET_BIT(TCCR0,WGM00);
+            CLEAR_BIT(TCCR0,WGM01);
+            break;
+		case TIMER0_CTC:
+            CLEAR_BIT(TCCR0,WGM00);
+            SET_BIT(TCCR0,WGM01);
+            break;
+		case TIMER0_FAST_PWM:
+            SET_BIT(TCCR0,WGM00);
+            SET_BIT(TCCR0,WGM01);
+            break;
+	}
+
+    /* clear LSB three bits for scaler */
+	TCCR0 &= 0XF8;
+	
+    /* add scaler to TCCR0 register */
     TCCR0 |= scaler;
 }
+void Timer0_OC0_Mode(TIMER0_OC0_type mode)
+{
+	switch (mode)
+	{
+		case OC0_DISCONNECTED:
+            CLEAR_BIT(TCCR0,COM00);
+            CLEAR_BIT(TCCR0,COM01);
+            break;
+		case OC0_TOGGLE:
+            SET_BIT(TCCR0,COM00);
+            CLEAR_BIT(TCCR0,COM01);
+            break;
+		case OC0_NON_INVERTING:
+            CLEAR_BIT(TCCR0,COM00);
+            SET_BIT(TCCR0,COM01);
+            break;
+		case OC0_INVERTING:
+            SET_BIT(TCCR0,COM00);
+            SET_BIT(TCCR0,COM01);
+            break;
+	}
+}
 
-void TIMER0_OvfIntEnable(void)
+void Timer0_Set(u8 time)
+{
+    TCNT0 = time;
+}
+u8   Timer0_Get(void)
+{
+    return TCNT0;
+}
+void Timer0_OCR_Set(u8 time)
+{
+    OCR0 = time;
+}
+
+void Timer0_OVF_IntEnable(void)
 {
     SET_BIT(TIMSK,TOIE0);
 }
-
-void TIMER0_OvfIntDisable(void)
+void Timer0_OVF_IntDisable(void)
 {
     CLEAR_BIT(TIMSK,TOIE0);
 }
-
-void TIMER0_ComIntEnable(void)
+void Timer0_OC_IntEnable(void)
 {
     SET_BIT(TIMSK,OCIE0);
 }
-
-void TIMER0_ComIntDisable(void)
+void Timer0_OC_IntDisable(void)
 {
     CLEAR_BIT(TIMSK,OCIE0);
 }
 
-void TIMER0_OvfIntSetCallBack(void(*LocalPtr)(void))
+void Timer0_OVF_IntSetCallBack(void(*LocalFptr)(void))
 {
-    TIMER0_OVF_IntFptr = LocalPtr;
+    TIMER0_OVF_IntFptr = LocalFptr;
+}
+void Timer0_OC_IntSetCallBack(void(*LocalFptr)(void))
+{
+    TIMER0_COM_IntFptr = LocalFptr;
 }
 
-void TIMER0_ComIntSetCallBack(void(*LocalPtr)(void))
+void Timer1_Init(TIMER1_Mode_type mode,TIMER1_Scaler_type scaler)
 {
-    TIMER0_COM_IntFptr = LocalPtr;
+    switch (mode)
+	{
+		case TIMER1_NORMAL_MODE:
+            CLEAR_BIT(TCCR1A,WGM10);
+            CLEAR_BIT(TCCR1A,WGM11);
+            CLEAR_BIT(TCCR1B,WGM12);
+            CLEAR_BIT(TCCR1B,WGM13);
+            break;
+		case TIMER1_CTC_ICR_TOP_MODE:
+            CLEAR_BIT(TCCR1A,WGM10);
+            CLEAR_BIT(TCCR1A,WGM11);
+            SET_BIT(TCCR1B,WGM12);
+            SET_BIT(TCCR1B,WGM13);
+            break;
+		case TIMER1_CTC_OCRA_TOP_MODE:
+            CLEAR_BIT(TCCR1A,WGM10);
+            CLEAR_BIT(TCCR1A,WGM11);
+            SET_BIT(TCCR1B,WGM12);
+            CLEAR_BIT(TCCR1B,WGM13);
+            break;
+		case TIMER1_FASTPWM_ICR_TOP_MODE:
+            CLEAR_BIT(TCCR1A,WGM10);
+            SET_BIT(TCCR1A,WGM11);
+            SET_BIT(TCCR1B,WGM12);
+            SET_BIT(TCCR1B,WGM13);
+            break;
+		case TIMER1_FASTPWM_OCRA_TOP_MODE:
+            SET_BIT(TCCR1A,WGM10);
+            SET_BIT(TCCR1A,WGM11);
+            SET_BIT(TCCR1B,WGM12);
+            SET_BIT(TCCR1B,WGM13);
+            break;
+	}
+
+    /* clear LSB three bits for scaler */
+	TCCR1B &= 0XF8;
+
+    /* add scaler to TCCR1B register */
+	TCCR1B|=scaler;
+}
+void Timer1_OCRA_Mode(TIMER1_OC1A_type oc1a_mode)
+{
+	switch (oc1a_mode)
+	{
+		case OCRA_DISCONNECTED:
+            CLEAR_BIT(TCCR1A,COM1A0);
+            CLEAR_BIT(TCCR1A,COM1A1);
+            break;
+		case OCRA_TOGGLE:
+            SET_BIT(TCCR1A,COM1A0);
+            CLEAR_BIT(TCCR1A,COM1A1);
+            break;
+		case OCRA_NON_INVERTING:
+            CLEAR_BIT(TCCR1A,COM1A0);
+            SET_BIT(TCCR1A,COM1A1);
+            break;
+		case OCRA_INVERTING:
+            SET_BIT(TCCR1A,COM1A0);
+            SET_BIT(TCCR1A,COM1A1);
+            break;
+	}
+}
+void Timer1_OCRB_Mode(TIMER1_OC1B_type oc1b_mode)
+{
+    switch (oc1b_mode)
+	{
+		case OCRB_DISCONNECTED:
+            CLEAR_BIT(TCCR1A,COM1B0);
+            CLEAR_BIT(TCCR1A,COM1B1);
+            break;
+		case OCRB_TOGGLE:
+            SET_BIT(TCCR1A,COM1B0);
+            CLEAR_BIT(TCCR1A,COM1B1);
+            break;
+		case OCRB_NON_INVERTING:
+            CLEAR_BIT(TCCR1A,COM1B0);
+            SET_BIT(TCCR1A,COM1B1);
+            break;
+		case OCRB_INVERTING:
+            SET_BIT(TCCR1A,COM1B0);
+            SET_BIT(TCCR1A,COM1B1);
+            break;
+	}
+}
+void Timer1_InputCaptureEdge(TIMER1_ICU_Edge_type edge)
+{
+    if(edge == RISING)
+        SET_BIT(TCCR1B,ICES1);
+	
+    else if(edge == FALLING)
+        CLEAR_BIT(TCCR1B,ICES1);
+}
+
+void Timer1_Set(u16 time)
+{
+    TCNT1 = time;
+}
+u16  Timer1_Get(void)
+{
+    return TCNT1;
+}
+void Timer1_OCRA_Set(u16 time)
+{
+    OCR1A = time;
+}
+void Timer1_OCRB_Set(u16 time)
+{
+    OCR1B = time;
+}
+void Timer1_ICR_Set(u16 time)
+{
+    ICR1 = time;
+}
+u16 Timer1_ICR_Get(void)
+{
+    return ICR1;
+}
+
+void Timer1_OVF_IntEnable(void)
+{
+    SET_BIT(TIMSK,TOIE1);
+}
+void Timer1_OVF_IntDisable(void)
+{
+    CLEAR_BIT(TIMSK,TOIE1);
+}
+void Timer1_OCA_IntEnable(void)
+{
+    SET_BIT(TIMSK,OCIE1A);
+}
+void Timer1_OCA_IntDisable(void)
+{
+    CLEAR_BIT(TIMSK,OCIE1A);
+}
+void Timer1_OCB_IntEnable(void)
+{
+    SET_BIT(TIMSK,OCIE1B);
+}
+void Timer1_OCB_IntDisable(void)
+{
+    CLEAR_BIT(TIMSK,OCIE1B);
+}
+void Timer1_ICU_IntEnable(void)
+{
+    SET_BIT(TIMSK,TICIE1);
+}
+void Timer1_ICU_IntDisable(void)
+{
+    CLEAR_BIT(TIMSK,TICIE1);
+}
+
+void Timer1_OVF_IntSetCallBack(void(*LocalFptr)(void))
+{
+    Timer1_OVF_IntFptr = LocalFptr;
+}
+void Timer1_OCA_IntSetCallBack(void(*LocalFptr)(void))
+{
+    Timer1_OCA_IntFptr = LocalFptr;
+}
+void Timer1_OCB_IntSetCallBack(void(*LocalFptr)(void))
+{
+    Timer1_OCB_IntFptr = LocalFptr;
+}
+void Timer1_ICU_IntSetCallBack(void(*LocalFptr)(void))
+{
+    Timer1_ICU_IntFptr = LocalFptr;
 }
 
 ISR(TIMER0_OVF_VECT)
@@ -101,11 +286,38 @@ ISR(TIMER0_OVF_VECT)
 		TIMER0_OVF_IntFptr();
 	}
 }
-
 ISR(TIMER0_COMP_VECT)
 {
 	if (TIMER0_COM_IntFptr != NULLPTR)
 	{
 		TIMER0_COM_IntFptr();
+	}
+}
+ISR(TIMER1_OVF_VECT)
+{
+	if(Timer1_OVF_IntFptr!=NULLPTR)
+	{
+		Timer1_OVF_IntFptr();
+	}
+}
+ISR(TIMER1_COMPA_VECT)
+{
+	if(Timer1_OCA_IntFptr!=NULLPTR)
+	{
+		Timer1_OCA_IntFptr();
+	}
+}
+ISR(TIMER1_COMPB_VECT)
+{
+	if(Timer1_OCB_IntFptr!=NULLPTR)
+	{
+		Timer1_OCB_IntFptr();
+	}
+}
+ISR(TIMER1_CAPT_VECT)
+{
+	if(Timer1_ICU_IntFptr!=NULLPTR)
+	{
+		Timer1_ICU_IntFptr();
 	}
 }
--- MCAL/Timer.h
+++ MCAL/Timer.h
@@ -23,7 +23,6 @@ typedef enum
     TIMER0_CTC,
     TIMER0_FAST_PWM
 }TIMER0_Mode_type;
-
 typedef enum
 {
     TIMER0_STOP = 0,
@@ -32,10 +31,9 @@ typedef enum
     TIMER0_SCALER_64,
     TIMER0_SCALER_256,
     TIMER0_SCALER_1024,
-    External_Clk_T0_FALLING,
-    External_Clk_T0_RISING
+    EXT_CLK_T0_FALLING,
+    EXT_CLK_T0_RISING
 }TIMER0_Scaler_type;
-
 typedef enum 
 {
     OC0_DISCONNECTED = 0,
@@ -44,15 +42,73 @@ typedef enum
     OC0_INVERTING
 }TIMER0_OC0_type;
 
+/**
+ * @brief Timer0 initialization 
+ * 
+ * @param mode TIMER0_NORMAL/TIMER0_PWM_PHASE_CORRECT/TIMER0_CTC/TIMER0_FAST_PWM
+ * @param scaler TIMER0_STOP/TIMER0_SCALER_1/TIMER0_SCALER_8
+    TIMER0_SCALER_64/TIMER0_SCALER_256/TIMER0_SCALER_1024
+    EXT_CLK_T0_FALLING/EXT_CLK_T0_RISING
+ */
 extern void Timer0_Init(TIMER0_Mode_type mode,TIMER0_Scaler_type scaler);
+/**
+ * @brief Timer0 OC0 pin mode
+ * 
+ * @param mode OC0_DISCONNECTED/OC0_TOGGLE/OC0_NON_INVERTING/OC0_INVERTING
+ */
 extern void Timer0_OC0_Mode(TIMER0_OC0_type mode);
 
-extern void Timer0_OVF_IntEnable(void);
-extern void Timer0_OVF_IntDisable(void);
-extern void Timer0_OVF_IntSetCallBack(void(*LocalFptr)(void));
+/**
+ * @brief Set TCNT0 value
+ * 
+ * @param time 0:255
+ */
+extern void Timer0_Set(u8 time);
+/**
+ * @brief Get TCNT0 value
+ * 
+ * @return u8 0:255
+ */
+extern u8 Timer0_Get(void);
+/**
+ * @brief Set OCR register value
+ * 
+ * @param time 0:255
+ */
+extern void Timer0_OCR_Set(u8 time);
 
+/**
+ * @brief Enable interrupt for T0 overflow
+ * 
+ */
+extern void Timer0_OVF_IntEnable(void);
+/**
+ * @brief Disable interrupt for T0 overflow
+ * 
+ */
+extern void Timer0_OVF_IntDisable(void);
+/**
+ * @brief Enable interrupt for T0 out compare match
+ * 
+ */
 extern void Timer0_OC_IntEnable(void);
+/**
+ * @brief Disable interrupt for T0 out compare match
+ * 
+ */
 extern void Timer0_OC_IntDisable(void);
+
+/**
+ * @brief set call back function for T0 overflow interrupt
+ * 
+ * @param LocalFptr address of function to run on interrupt
+ */
+extern void Timer0_OVF_IntSetCallBack(void(*LocalFptr)(void));
+/**
+ * @brief Set call back function for T0 out compare match interrupt
+ * 
+ * @param LocalFptr address fo function to run on interrupt
+ */
 extern void Timer0_OC_IntSetCallBack(void(*LocalFptr)(void));
 
 typedef enum
@@ -63,10 +119,9 @@ typedef enum
 	TIMER1_SCALER_64,
 	TIMER1_SCALER_256,
 	TIMER1_SCALER_1024,
-	EXTERNAL0_FALLING,
-	EXTERNAL0_RISING
+	EXT_CLK_T1_FALLING,
+	EXT_CLK_T1_RISING
 }TIMER1_Scaler_type;
-
 typedef enum
 {
 	TIMER1_NORMAL_MODE=0,
@@ -75,7 +130,6 @@ typedef enum
 	TIMER1_FASTPWM_ICR_TOP_MODE,
 	TIMER1_FASTPWM_OCRA_TOP_MODE
 }TIMER1_Mode_type;
-
 typedef enum
 {
 	OCRA_DISCONNECTED=0,
@@ -83,7 +137,6 @@ typedef enum
 	OCRA_NON_INVERTING,
 	OCRA_INVERTING
 }TIMER1_OC1A_type;
-
 typedef enum
 {
 	OCRB_DISCONNECTED=0,
@@ -92,33 +145,142 @@ typedef enum
 	OCRB_INVERTING
 
 }TIMER1_OC1B_type;
-
 typedef enum
 {
 	RISING,
 	FALLING
 }TIMER1_ICU_Edge_type;
 
+/**
+ * @brief Timer1 initialization
+ * 
+ * @param mode TIMER1_NORMAL_MODE/TIMER1_CTC_ICR_TOP_MODE/TIMER1_CTC_OCRA_TOP_MODE
+	TIMER1_FASTPWM_ICR_TOP_MODE/TIMER1_FASTPWM_OCRA_TOP_MODE
+ * @param scaler TIMER1_STOP/TIMER1_SCALER_1/TIMER1_SCALER_8/TIMER1_SCALER_64
+	TIMER1_SCALER_256/TIMER1_SCALER_1024/EXT_CLK_T1_FALLING/EXT_CLK_T1_RISING
+ */
 extern void Timer1_Init(TIMER1_Mode_type mode,TIMER1_Scaler_type scaler);
-extern void Timer1_OCRA1Mode(TIMER1_OC1A_type oc1a_mode);
-extern void Timer1_OCRB1Mode(TIMER1_OC1B_type oc1b_mode);
-
-extern void Timer1_OVF_IntEnable(void);
-extern void Timer1_OVF_IntDisable(void);
-extern void Timer1_OVF_IntSetCallBack(void(*LocalFptr)(void));
-
-extern void Timer1_OCA_IntEnable(void);
-extern void Timer1_OCA_IntDisable(void);
-extern void Timer1_OCA_IntSetCallBack(void(*LocalFptr)(void));
-
-extern void Timer1_OCB_IntEnable(void);
-extern void Timer1_OCB_IntDisable(void);
-extern void Timer1_OCB_IntSetCallBack(void(*LocalFptr)(void));
-
+/**
+ * @brief Set oc1a pin mode
+ * 
+ * @param oc1a_mode OCRA_DISCONNECTED/OCRA_TOGGLE/OCRA_NON_INVERTING/OCRA_INVERTING
+ */
+extern void Timer1_OCRA_Mode(TIMER1_OC1A_type oc1a_mode);
+/**
+ * @brief Set oc1b pin mode
+ * 
+ * @param oc1b_mode OCRB_DISCONNECTED/OCRB_TOGGLE/OCRB_NON_INVERTING/OCRB_INVERTING
+ */
+extern void Timer1_OCRB_Mode(TIMER1_OC1B_type oc1b_mode);
+/**
+ * @brief Set edge for input capture unit in timer1
+ * 
+ * @param edge RISING/FALLING
+ */
 extern void Timer1_InputCaptureEdge(TIMER1_ICU_Edge_type edge);
 
+/**
+ * @brief Set timer1 value
+ * 
+ * @param time 0:65535
+ */
+extern void Timer1_Set(u16 time);
+/**
+ * @brief Get timer1 value
+ * 
+ * @return u16 0:65535
+ */
+extern u16 Timer1_Get(void);
+/**
+ * @brief Set OCRA register value
+ * 
+ * @param time 0:65535
+ */
+extern void Timer1_OCRA_Set(u16 time);
+/**
+ * @brief Set OCRB register value
+ * 
+ * @param time 0:65535
+ */
+extern void Timer1_OCRB_Set(u16 time);
+/**
+ * @brief Set ICR register value
+ * 
+ * @param time 0:65535
+ */
+extern void Timer1_ICR_Set(u16 time);
+/**
+ * @brief Get ICR1 register value
+ * 
+ * @return u16 0:65535
+ */
+extern u16 Timer1_ICR_Get(void);
+
+
+/**
+ * @brief Enable Timer1 overflow interrupt
+ * 
+ */
+extern void Timer1_OVF_IntEnable(void);
+/**
+ * @brief Disable Timer1 overflow interrupt
+ * 
+ */
+extern void Timer1_OVF_IntDisable(void);
+/**
+ * @brief Enable OCA interrupt of Timer1
+ * 
+ */
+extern void Timer1_OCA_IntEnable(void);
+/**
+ * @brief Disable OCA interrupt of Timer1
+ * 
+ */
+extern void Timer1_OCA_IntDisable(void);
+/**
+ * @brief Enable OCB interrupt for Timer1
+ * 
+ */
+extern void Timer1_OCB_IntEnable(void);
+/**
+ * @brief Disable OCB interrupt for Timer1
+ * 
+ */
+extern void Timer1_OCB_IntDisable(void);
+/**
+ * @brief Enable input capture interrupt
+ * 
+ */
 extern void Timer1_ICU_IntEnable(void);
+/**
+ * @brief Disable input capture interrupt
+ * 
+ */
 extern void Timer1_ICU_IntDisable(void);
+
+/**
+ * @brief Set call back function for T1 overflow interrupt 
+ * 
+ * @param LocalFptr Address of function to run on interrupt 
+ */
+extern void Timer1_OVF_IntSetCallBack(void(*LocalFptr)(void));
+/**
+ * @brief Set call back function of OCA interrupt of timer1
+ * 
+ * @param LocalFptr Address to function to run on OCA interrupt
+ */
+extern void Timer1_OCA_IntSetCallBack(void(*LocalFptr)(void));
+/**
+ * @brief Set call back function for OCB interrupt
+ * 
+ * @param LocalFptr Address of function to run on OCB interrupt
+ */
+extern void Timer1_OCB_IntSetCallBack(void(*LocalFptr)(void));
+/**
+ * @brief Set call back function for input capture interrupt
+ * 
+ * @param LocalFptr address to function to run on input capture interrupt
+ */
 extern void Timer1_ICU_IntSetCallBack(void(*LocalFptr)(void));
 
 #endif /* TIMER_H_ */
