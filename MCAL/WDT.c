f5546abc6e6d359aa79fde9e145a95403c217166
AhmedSabry10696 <ahmed.sabry10696@gmail.com>
2021-07-15 02:32:00 +0200

    Watch Dog Timer Driver

---
+++ MCAL/WDT.c
@@ -0,0 +1,30 @@
+/**
+ * @file WDT.c
+ * @author Ahmed Sabry (ahmed.sabry10696@gmail.com)
+ * @brief watch dog timer driver
+ * @version 0.1
+ * @date 15-07-2021
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include "WDT.h"
+
+void WDT_Set(TimeOut_type time)
+{
+    u8 wdt_value = 0x08;
+    wdt_value |= time;
+    
+    WDTCR |= (1<<WDTOE)|(1<<WDE);
+    WDTCR = wdt_value;
+}
+void WDT_Stop(void)
+{
+    WDTCR = (1<<WDTOE)|(1<<WDE);
+
+    cli();
+    /* with in only 4 cycles write 0 */
+    WDTCR = 0;
+    sei();
+}
---
+++ MCAL/WDT.h
@@ -0,0 +1,34 @@
+/**
+ * @file WDT.h
+ * @author Ahmed Sabry (ahmed.sabry10696@gmail.com)
+ * @brief Watch dog timer header file
+ * @version 0.1
+ * @date 15-07-2021
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef WDT_H_
+#define WDT_H_
+
+#include "StdTypes.h"
+#include "MemMap.h"
+#include "Utils.h"
+
+typedef enum
+{
+    TimeOut_16ms=0,
+    TimeOut_32ms,
+    TimeOut_65ms,
+    TimeOut_130ms,
+    TimeOut_260ms,
+    TimeOut_520ms,
+    TimeOut_1000ms,
+    TimeOut_2100ms 
+}TimeOut_type;
+
+extern void WDT_Set(TimeOut_type time);
+extern void WDT_Stop(void);
+
+#endif
