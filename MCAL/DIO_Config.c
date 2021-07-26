02a1ad9660350f8d73e92e164b2bd4e2ee2c972d
AhmedSabry10696 <ahmed.sabry10696@gmail.com>
2021-07-26 21:17:52 +0200

    DIO config file

--- MCAL/DIO_Config.c
+++ MCAL/DIO_Config.c
@@ -14,42 +14,42 @@
 const DIO_PinStatus_type PinsStatus[TOTAL_PINS] = 
 {
     /* PORTA PINS 0:7*/
-    INFREE,
-    INFREE,
-    OUTPUT,
-    OUTPUT,
-    OUTPUT,
-    OUTPUT,
-    OUTPUT,
-    OUTPUT,
+    OUTPUT, /* ADC0 */
+    OUTPUT, /* ADC1 */
+    OUTPUT, /* ADC2 */
+    OUTPUT, /* ADC3 */
+    OUTPUT, /* ADC4 */
+    OUTPUT, /* ADC5 */
+    OUTPUT, /* ADC6 */
+    OUTPUT, /* ADC7 */
 
     /* PORTB PINS 0:7*/
-    OUTPUT,
-    OUTPUT,
-    OUTPUT,
-    OUTPUT,
-    OUTPUT,
-    OUTPUT,
-    OUTPUT,
-    OUTPUT,
+    OUTPUT, /* XCK/T0 {usart}/{T0 pin ext clock/counter} */
+    OUTPUT, /* T1     {T1 pin ext clock/counter} */
+    OUTPUT, /* INT2   {external interrupt 2} */
+    OUTPUT, /* OC0    {output compare pin T0} */
+    OUTPUT, /* SS     {SPI} */
+    OUTPUT, /* MOSI   {SPI} */
+    INFREE, /* MISO   {SPI} */ 
+    OUTPUT, /* SCK    {SPI} */
 
     /* PORTC PINS 0:7*/    
-    OUTPUT,
-    OUTPUT,
-    OUTPUT,
-    OUTPUT,
-    OUTPUT,
-    OUTPUT,
-    OUTPUT,
-    OUTPUT,
+    OUTPUT, /* SCL {I2C} */
+    OUTPUT, /* SDA {I2C} */
+    OUTPUT, /* TCK */
+    OUTPUT, /* TMS */
+    OUTPUT, /* TDO */
+    OUTPUT, /* TDI */
+    OUTPUT, /* TOSC1  {T2} */
+    OUTPUT, /* TOSC2  {T2} */
 
     /* PORTD PINS 0:7*/
-    INFREE,
-    OUTPUT,
-    OUTPUT,
-    OUTPUT,
-    OUTPUT,
-    OUTPUT,
-    OUTPUT,
-    OUTPUT
+    INFREE, /* RXD  {uart} */
+    OUTPUT, /* TXD  {uart} */
+    OUTPUT, /* INT0 {external interrupt 0} */
+    OUTPUT, /* INT1 {external interrupt 1} */
+    OUTPUT, /* OC1B {output compare B T1} */ 
+    OUTPUT, /* OC1A {output compare A T1} */
+    OUTPUT, /* ICP1 {input capture pin T1} */
+    OUTPUT  /* OC2  {output compare pin T2} */
 };
