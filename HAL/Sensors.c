01028751f464115b8de7b77f4212b7d861db315e
AhmedSabry10696 <ahmed.sabry10696@gmail.com>
2021-07-30 02:55:12 +0200

    sensors driver

--- HAL/Sensors.c
+++ HAL/Sensors.c
@@ -14,7 +14,7 @@
 u16 POT_VoltRead(void)
 {
     u16 adc = ADC_Read(POT);
-    u16 volt = ((u32)adc*5000)/1024;
+    u16 volt = ((u32)adc * 5000) / 1024;
 
     return volt;
 }
@@ -23,9 +23,9 @@ u16 POT_VoltRead(void)
 u16 LM35_TempRead(void)
 {
     u16 adc = ADC_Read(LM35);
-    u16 volt = ((u32)adc*5000)/1024;
+    u16 volt = ((u32)adc * 5000) / 1024;
     u16 temp = volt;
-    
+
     return temp;
 }
 
@@ -37,6 +37,6 @@ u16 LM35_TempRead(void)
 u16 MPX4110_PressureRead(void)
 {
     u16 adc = ADC_Read(MPX4110);
-    u16 pres = (((adc-55)*(u32)1000)/921)+150;
+    u16 pres = (((adc - 55) * (u32)1000) / 921) + 150;
     return pres;
 }
