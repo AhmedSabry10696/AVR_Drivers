d4810f74cb621808729a89e726d21b6e490b511c
AhmedSabry10696 <ahmed.sabry10696@gmail.com>
2021-07-30 02:56:07 +0200

    Memory map file

--- LIB/MemMap.h
+++ LIB/MemMap.h
@@ -297,19 +297,19 @@ void vector (void)
 
 /************** SPI REG **************/
 #define SPCR    (*(volatile u8*)0x2D)
-#define SPIE         7
-#define SPE          6
-#define DORD         5
-#define MSTR         4
-#define CPOL         3
-#define CPHA         2
-#define SPR1         1
-#define SPR0         0
+#define SPIE    7
+#define SPE     6
+#define DORD    5
+#define MSTR    4
+#define CPOL    3
+#define CPHA    2
+#define SPR1    1
+#define SPR0    0
 
 #define SPSR    (*(volatile u8*)0x2E)
-#define SPIF         7
-#define WCOL         6
-#define SPI2X        0
+#define SPIF    7
+#define WCOL    6
+#define SPI2X   0
 
 #define SPDR    (*(volatile u8*)0x2F)
 
