081ce6c6706e649746898c015d8364f9322fb1d6
AhmedSabry10696 <ahmed.sabry10696@gmail.com>
2021-07-30 02:55:55 +0200

    Ultrasonic driver

--- HAL/UltraSonic.c
+++ HAL/UltraSonic.c
@@ -11,7 +11,7 @@
 
 #include "UltraSonic.h"
 
-static volatile u16 t1,t2;
+static volatile u16 t1, t2;
 static volatile u8 flag = 3;
 
 static void ICU_Func(void)
@@ -30,24 +30,25 @@ static void ICU_Func(void)
 }
 void UlTRASONIC_Init(void)
 {
-    Timer1_Init(TIMER1_NORMAL_MODE,TIMER1_SCALER_8);
+    Timer1_Init(TIMER1_NORMAL_MODE, TIMER1_SCALER_8);
     Timer1_ICU_IntSetCallBack(ICU_Func);
 }
 u16 ULTRASONIC_Read(ULTRASONIC_type ultra)
 {
-    u16 d,time;
+    u16 d, time;
     Timer1_ICU_IntEnable();
     flag = 0;
     Timer1_InputCaptureEdge(RISING);
-    
-    /* trigger pin */
-    DIO_WritePin(ultra,HIGH);
-    _delay_us(10);
-    DIO_WritePin(ultra,LOW);
 
-    while (flag < 2);
-    time = t2-t1;
-    d = time/58;
+    /* trigger pin */
+    DIO_WritePin(ultra, HIGH);
+    _delay_us(10);
+    DIO_WritePin(ultra, LOW);
+
+    while (flag < 2)
+        ;
+    time = t2 - t1;
+    d = time / 58;
     return d;
 }
 void ULTRASONIC_Start(ULTRASONIC_type ultra)
@@ -59,9 +60,9 @@ void ULTRASONIC_Start(ULTRASONIC_type ultra)
         Timer1_ICU_IntEnable();
 
         /* trigger pin */
-        DIO_WritePin(ultra,HIGH);
+        DIO_WritePin(ultra, HIGH);
         _delay_us(10);
-        DIO_WritePin(ultra,LOW);
+        DIO_WritePin(ultra, LOW);
     }
 }
 u8 ULTRASONIC_GetReadNoBlock(u16 *pdistance)
@@ -69,8 +70,8 @@ u8 ULTRASONIC_GetReadNoBlock(u16 *pdistance)
     u16 time;
     if (2 == flag)
     {
-        time = t2-t1;
-        *pdistance = time/58;
+        time = t2 - t1;
+        *pdistance = time / 58;
         flag = 3;
         return 1;
     }
