aac0a9eedd21742f67d8d822e5dc5e5f9afc8fff
AhmedSabry10696 <ahmed.sabry10696@gmail.com>
2021-07-31 02:00:33 +0200

    APP

--- main.c
+++ main.c
@@ -8,14 +8,22 @@
 #include "Ext_Interrupt.h"
 #include "Uart.h"
 #include "LCD.h"
-#include "I2C_Services.h"
+#include "EXT_EEPROM.h"
 
 int main(void)
 {
 	sei();
 	DIO_Init();
 	LCD_Init();	
-	TWI_Init();
+	EEPROM_Init();
+	u8 y = 0;
+	
+	EEPROM_Write_Byte(512,10);
+	_delay_ms(10);
+	EEPROM_Read_Byte(512,&y);
+		
+	LCD_GoTo(2,0);
+	LCD_WriteNum(y);
 	
 	while(1)
 	{	
