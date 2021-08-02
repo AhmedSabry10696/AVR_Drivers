3eec470a0272cff52c9adf8a009990b5d0208dff
AhmedSabry10696 <ahmed.sabry10696@gmail.com>
2021-08-02 21:40:58 +0200

    dio config file

--- MCAL/dio_config.c
+++ MCAL/dio_config.c
@@ -13,43 +13,47 @@
 
 const DioPinStatus_type pins_status[TOTAL_PINS] = 
 {
-    /* PORTA PINS 0:7*/
-    OUTPUT, /* ADC0 */
-    OUTPUT, /* ADC1 */
-    OUTPUT, /* ADC2 */
-    OUTPUT, /* ADC3 */
-    OUTPUT, /* ADC4 */
-    OUTPUT, /* ADC5 */
-    OUTPUT, /* ADC6 */
-    OUTPUT, /* ADC7 */
+    /****** PORTA PINS 0:7 ******/
+    OUTPUT,     /* ADC0         */
+    OUTPUT,     /* ADC1         */
+    OUTPUT,     /* ADC2         */
+    OUTPUT,     /* ADC3         */
+    OUTPUT,     /* ADC4         */
+    OUTPUT,     /* ADC5         */
+    OUTPUT,     /* ADC6         */
+    OUTPUT,     /* ADC7         */
+    /****************************/
 
-    /* PORTB PINS 0:7*/
-    OUTPUT, /* XCK/T0 {usart}/{T0 pin ext clock/counter} */
-    OUTPUT, /* T1     {T1 pin ext clock/counter} */
-    OUTPUT, /* INT2   {external interrupt 2} */
-    OUTPUT, /* OC0    {output compare pin T0} */
-    OUTPUT, /* SS     {SPI} */
-    OUTPUT, /* MOSI   {SPI} */
-    OUTPUT, /* MISO   {SPI INFREE} */ 
-    OUTPUT, /* SCK    {SPI} */
+    /********************** PORTB PINS 0:7 **************************/
+    OUTPUT,     /* XCK/T0 {usart clock}/{T0 pin ext clock/counter}  */
+    OUTPUT,     /* T1     {T1 pin ext clock/counter}                */
+    OUTPUT,     /* INT2   {ext interrupt 2}                         */
+    OUTPUT,     /* OC0    {output compare pin T0}                   */
+    OUTPUT,     /* SS     {SPI OUTPUT(master),INFREE(slave)}        */
+    OUTPUT,     /* MOSI   {SPI OUTPUT(master),INFREE(slave)}        */
+    OUTPUT,     /* MISO   {SPI INFREE(master),OUTPUT(slave)}        */ 
+    OUTPUT,     /* SCK    {SPI OUTPUT(master),INFREE(slave)}        */
+    /****************************************************************/
 
-    /* PORTC PINS 0:7*/    
-    INFREE, /* SCL {I2C} */
-    INFREE, /* SDA {I2C} */
-    OUTPUT, /* TCK */
-    OUTPUT, /* TMS */
-    OUTPUT, /* TDO */
-    OUTPUT, /* TDI */
-    OUTPUT, /* TOSC1  {T2} */
-    OUTPUT, /* TOSC2  {T2} */
+    /********** PORTC PINS 0:7 **********/    
+    INFREE,     /* SCL {I2C INFREE}     */
+    INFREE,     /* SDA {I2C INFREE}     */
+    OUTPUT,     /* TCK                  */
+    OUTPUT,     /* TMS                  */
+    OUTPUT,     /* TDO                  */
+    OUTPUT,     /* TDI                  */
+    OUTPUT,     /* TOSC1  {T2}          */
+    OUTPUT,     /* TOSC2  {T2}          */
+    /************************************/
 
-    /* PORTD PINS 0:7*/
-    OUTPUT, /* RXD  {uart INFREE} */
-    OUTPUT, /* TXD  {uart OUTPUT} */
-    OUTPUT, /* INT0 {external interrupt 0} */
-    OUTPUT, /* INT1 {external interrupt 1} */
-    OUTPUT, /* OC1B {output compare B T1} */ 
-    OUTPUT, /* OC1A {output compare A T1} */
-    OUTPUT, /* ICP1 {input capture pin T1} */
-    OUTPUT  /* OC2  {output compare pin T2} */
+    /*************** PORTD PINS 0:7 *************/
+    OUTPUT,     /* RXD  {uart INFREE}           */
+    OUTPUT,     /* TXD  {uart OUTPUT}           */
+    OUTPUT,     /* INT0 {ext interrupt 0}       */
+    OUTPUT,     /* INT1 {ext interrupt 1}       */
+    OUTPUT,     /* OC1B {output compare B T1}   */ 
+    OUTPUT,     /* OC1A {output compare A T1}   */
+    OUTPUT,     /* ICP1 {input capture pin T1}  */
+    OUTPUT      /* OC2  {output compare pin T2} */
+    /********************************************/
 };
