95c0c8ef90cfa647905ce83b69bdfd035065c203
AhmedSabry10696 <ahmed.sabry10696@gmail.com>
2021-08-02 19:11:24 +0200

    lcd driver

--- HAL/LCD.c
+++ HAL/LCD.c
@@ -1,5 +1,5 @@
 /**
- * @file LCD.c
+ * @file lcd.c
  * @author Ahmed Sabry (ahmed.sabry10696@gmail.com)
  * @brief LCD driver implementation
  * @version 0.1
@@ -13,175 +13,175 @@
 
 #if (8 == DATA_BITS_MODE)
 
-static void LCD_SendCommand(u8 command)
+static void LCD_sendCommand(u8 command)
 {
 	/* out the required command to the data bus D0:D7 */
-	DIO_WritePort(LCD_DATA_PORT, command);
+	DIO_writePort(LCD_DATA_PORT, command);
 
 	/* Instruction Mode RS = 0 */
-	DIO_WritePin(RS, LOW);
+	DIO_writePin(RS, LOW);
 
 	/* write data to LCD so RW = 0 */
-	DIO_WritePin(RW, LOW);
+	DIO_writePin(RW, LOW);
 
 	/* Enable LCD E = 1 */
-	DIO_WritePin(E, HIGH);
+	DIO_writePin(E, HIGH);
 	_delay_ms(1);
 
 	/* Disable LCD E = 0 */
-	DIO_WritePin(E, LOW);
+	DIO_writePin(E, LOW);
 	_delay_ms(1);
 }
 
-static void LCD_SendData(u8 data)
+static void LCD_sendData(u8 data)
 {
 	/* out the required command to the data bus D0:D7 */
-	DIO_WritePort(LCD_DATA_PORT, data);
+	DIO_writePort(LCD_DATA_PORT, data);
 
 	/* Data Mode RS = 1 */
-	DIO_WritePin(RS, HIGH);
+	DIO_writePin(RS, HIGH);
 
 	/* write data to LCD so RW = 0 */
-	DIO_WritePin(RW, LOW);
+	DIO_writePin(RW, LOW);
 
 	/* Enable LCD E = 1 */
-	DIO_WritePin(E, HIGH);
+	DIO_writePin(E, HIGH);
 	_delay_ms(1);
 
 	/* Disable LCD E = 0 */
-	DIO_WritePin(E, LOW);
+	DIO_writePin(E, LOW);
 	_delay_ms(1);
 }
 
-void LCD_Init(void)
+void LCD_init(void)
 {
 	/* wait 20ms after vdd reaches 4.5 volt */
 	_delay_ms(20);
 
 	/* use 2-line lcd + 8-bit Data Mode + 5*7 dot display Mode */
-	LCD_SendCommand(TWO_LINE_LCD_Eight_BIT_MODE);
+	LCD_sendCommand(TWO_LINE_LCD_Eight_BIT_MODE);
 
 	/* Cursor Off */
-	LCD_SendCommand(CURSOR_OFF);
+	LCD_sendCommand(CURSOR_OFF);
 
 	/* Clear Screen */
-	LCD_SendCommand(CLEAR_COMMAND);
+	LCD_sendCommand(CLEAR_COMMAND);
 
 	_delay_ms(2);
 }
 
 #elif (4 == DATA_BITS_MODE)
 
-static void LCD_SendCommand(u8 command)
+static void LCD_sendCommand(u8 command)
 {
 	/* write Most significant four bits on D4:D7 */
-	DIO_WritePin(D7, READ_BIT(command, 7));
-	DIO_WritePin(D6, READ_BIT(command, 6));
-	DIO_WritePin(D5, READ_BIT(command, 5));
-	DIO_WritePin(D4, READ_BIT(command, 4));
+	DIO_writePin(D7, READ_BIT(command, 7));
+	DIO_writePin(D6, READ_BIT(command, 6));
+	DIO_writePin(D5, READ_BIT(command, 5));
+	DIO_writePin(D4, READ_BIT(command, 4));
 
 	/* Command mode RS = 0 */
-	DIO_WritePin(RS, LOW);
+	DIO_writePin(RS, LOW);
 
 	/* Write mode RW = 0 */
-	DIO_WritePin(RW, LOW);
+	DIO_writePin(RW, LOW);
 
 	/* LCD Enable E = 1 */
-	DIO_WritePin(E, HIGH);
+	DIO_writePin(E, HIGH);
 	_delay_ms(1);
 
 	/* LCD disable E = 0 */
-	DIO_WritePin(E, LOW);
+	DIO_writePin(E, LOW);
 	_delay_ms(1);
 
 	/* write LOW significant four bits on D4:D7 */
-	DIO_WritePin(D7, READ_BIT(command, 3));
-	DIO_WritePin(D6, READ_BIT(command, 2));
-	DIO_WritePin(D5, READ_BIT(command, 1));
-	DIO_WritePin(D4, READ_BIT(command, 0));
+	DIO_writePin(D7, READ_BIT(command, 3));
+	DIO_writePin(D6, READ_BIT(command, 2));
+	DIO_writePin(D5, READ_BIT(command, 1));
+	DIO_writePin(D4, READ_BIT(command, 0));
 
 	/* LCD Enable E = 1 */
-	DIO_WritePin(E, HIGH);
+	DIO_writePin(E, HIGH);
 	_delay_ms(1);
 
 	/* LCD Disable E = 0 */
-	DIO_WritePin(E, LOW);
+	DIO_writePin(E, LOW);
 	_delay_ms(1);
 }
 
-static void LCD_SendData(u8 data)
+static void LCD_sendData(u8 data)
 {
 	/* Write most significant 4 bits on D4:D7 */
-	DIO_WritePin(D7, READ_BIT(data, 7));
-	DIO_WritePin(D6, READ_BIT(data, 6));
-	DIO_WritePin(D5, READ_BIT(data, 5));
-	DIO_WritePin(D4, READ_BIT(data, 4));
+	DIO_writePin(D7, READ_BIT(data, 7));
+	DIO_writePin(D6, READ_BIT(data, 6));
+	DIO_writePin(D5, READ_BIT(data, 5));
+	DIO_writePin(D4, READ_BIT(data, 4));
 
 	/* Data mode RS = 1 */
-	DIO_WritePin(RS, HIGH);
+	DIO_writePin(RS, HIGH);
 
 	/* LCD enable E = 1 */
-	DIO_WritePin(E, HIGH);
+	DIO_writePin(E, HIGH);
 	_delay_ms(1);
 
 	/* LCD disable E = 0 */
-	DIO_WritePin(E, LOW);
+	DIO_writePin(E, LOW);
 	_delay_ms(1);
 
 	/* write LOW significant 4 bits on D4:D7 */
-	DIO_WritePin(D7, READ_BIT(data, 3));
-	DIO_WritePin(D6, READ_BIT(data, 2));
-	DIO_WritePin(D5, READ_BIT(data, 1));
-	DIO_WritePin(D4, READ_BIT(data, 0));
+	DIO_writePin(D7, READ_BIT(data, 3));
+	DIO_writePin(D6, READ_BIT(data, 2));
+	DIO_writePin(D5, READ_BIT(data, 1));
+	DIO_writePin(D4, READ_BIT(data, 0));
 
 	/* LCD Enable E = 1 */
-	DIO_WritePin(E, HIGH);
+	DIO_writePin(E, HIGH);
 	_delay_ms(1);
 
 	/* LCD disable E = 0 */
-	DIO_WritePin(E, LOW);
+	DIO_writePin(E, LOW);
 	_delay_ms(1);
 }
 
-void LCD_Init(void)
+void LCD_init(void)
 {
 	/* wait for 15ms after vdd goes to 4.5 volt */
 	_delay_ms(15);
 
 	/* initialize LCD in 4-bit mode */
-	LCD_SendCommand(FOUR_BITS_DATA_MODE);
+	LCD_sendCommand(FOUR_BITS_DATA_MODE);
 
 	/* use 2-line lcd + 4-bit Data Mode + 5*7 dot display Mode */
-	LCD_SendCommand(TWO_LINE_LCD_Four_BIT_MODE);
+	LCD_sendCommand(TWO_LINE_LCD_Four_BIT_MODE);
 
 	/* cursor off */
-	LCD_SendCommand(CURSOR_OFF);
+	LCD_sendCommand(CURSOR_OFF);
 
 	/* clear screen */
-	LCD_SendCommand(CLEAR_COMMAND);
+	LCD_sendCommand(CLEAR_COMMAND);
 
 	_delay_ms(2);
 }
 #endif
 
-void LCD_WriteChar(const u8 ch)
+void LCD_writeChar(const u8 ch)
 {
-	LCD_SendData(ch);
+	LCD_sendData(ch);
 }
 
-void LCD_WriteString(const u8 *str)
+void LCD_writeString(const u8 *str)
 {
 	u8 i = 0;
 
 	/* loop till null */
 	while (str[i] != 0)
 	{
-		LCD_WriteChar(str[i]);
+		LCD_writeChar(str[i]);
 		i++;
 	}
 }
 
-void LCD_GoTo(u8 row, u8 col)
+void LCD_goTo(u8 row, u8 col)
 {
 	u8 Address = 0;
 
@@ -210,33 +210,33 @@ void LCD_GoTo(u8 row, u8 col)
 	}
 
 	/* to set cursor location apply this operation 0x80|address */
-	LCD_SendCommand(Address | SET_CURSOR_LOCATION);
+	LCD_sendCommand(Address | SET_CURSOR_LOCATION);
 }
 
-void LCD_WriteStringRowCol(u8 row, u8 col, const u8 *str)
+void LCD_writeStringRowCol(u8 row, u8 col, const u8 *str)
 {
 	/* go to to the required LCD position */
-	LCD_GoTo(row, col);
+	LCD_goTo(row, col);
 
 	/* display the string */
-	LCD_WriteString(str);
+	LCD_writeString(str);
 }
 
-void LCD_WriteNum(s32 num)
+void LCD_writeNum(s32 num)
 {
 	u8 i = 0, j, digit, str[10];
 
 	/* if number 0 */
 	if (0 == num)
 	{
-		LCD_WriteChar('0');
+		LCD_writeChar('0');
 		return;
 	}
 
 	/* if the number is negative */
 	if (num < 0)
 	{
-		LCD_WriteChar('-');
+		LCD_writeChar('-');
 
 		/* convert to positive form */
 		num = num * -1;
@@ -254,20 +254,20 @@ void LCD_WriteNum(s32 num)
 	/* print str on lcd */
 	for (j = i; j > 0; j--)
 	{
-		LCD_WriteChar(str[j - 1]);
+		LCD_writeChar(str[j - 1]);
 	}
 }
 
-void LCD_WriteFloat(f32 num)
+void LCD_writeFloat(f32 num)
 {
 	s32 left = (s32)num;
 	u8 right = (f32)(num - left) * 100;
-	LCD_WriteNum(left);
-	LCD_WriteChar('.');
-	LCD_WriteNum(right);
+	LCD_writeNum(left);
+	LCD_writeChar('.');
+	LCD_writeNum(right);
 }
 
-void LCD_WriteBinary(u16 num)
+void LCD_writeBinary(u16 num)
 {
 	s8 i, flag = 0;
 	for (i = 15; i >= 0; i--)
@@ -275,19 +275,19 @@ void LCD_WriteBinary(u16 num)
 		/* check if the bit is 1 or 0 */
 		if (1 == ((num >> i) & 0x01))
 		{
-			LCD_WriteChar('1');
+			LCD_writeChar('1');
 			flag = 1;
 		}
 
 		/* ignore left side zero's */
 		else if (flag == 1)
 		{
-			LCD_WriteChar('0');
+			LCD_writeChar('0');
 		}
 	}
 }
 
-void LCD_WriteHex(u16 num)
+void LCD_writeHex(u16 num)
 {
 	u8 digit4 = ((num & 0xF000) >> 12);
 	u8 digit3 = ((num & 0x0F00) >> 8);
@@ -296,68 +296,68 @@ void LCD_WriteHex(u16 num)
 
 	if (digit4 > 9)
 	{
-		LCD_WriteChar(digit4 + 'A' - 10);
+		LCD_writeChar(digit4 + 'A' - 10);
 	}
 	else
 	{
-		LCD_WriteChar(digit4 + '0');
+		LCD_writeChar(digit4 + '0');
 	}
 
 	if (digit3 > 9)
 	{
-		LCD_WriteChar(digit3 + 'A' - 10);
+		LCD_writeChar(digit3 + 'A' - 10);
 	}
 	else
 	{
-		LCD_WriteChar(digit3 + '0');
+		LCD_writeChar(digit3 + '0');
 	}
 
 	if (digit2 > 9)
 	{
-		LCD_WriteChar(digit2 + 'A' - 10);
+		LCD_writeChar(digit2 + 'A' - 10);
 	}
 	else
 	{
-		LCD_WriteChar(digit2 + '0');
+		LCD_writeChar(digit2 + '0');
 	}
 
 	if (digit1 > 9)
 	{
-		LCD_WriteChar(digit1 + 'A' - 10);
+		LCD_writeChar(digit1 + 'A' - 10);
 	}
 	else
 	{
-		LCD_WriteChar(digit1 + '0');
+		LCD_writeChar(digit1 + '0');
 	}
 }
 
-void LCD_Create_Char(const u8 *Pattern, const u8 Location)
+void LCD_createChar(const u8 *Pattern, const u8 Location)
 {
 	u8 i = 0;
 
 	/* send address format of GCRAM */
-	LCD_SendCommand(0x40 + (Location * 8));
+	LCD_sendCommand(0x40 + (Location * 8));
 
 	for (i = 0; i < 8; i++)
 	{
-		LCD_SendData(Pattern[i]);
+		LCD_sendData(Pattern[i]);
 	}
 
 	/* set cursor location to the first index of lcd */
-	LCD_SendCommand(SET_CURSOR_LOCATION);
+	LCD_sendCommand(SET_CURSOR_LOCATION);
 }
 
-void LCD_Clear(void)
+void LCD_clear(void)
 {
-	LCD_SendCommand(CLEAR_COMMAND);
+	LCD_sendCommand(CLEAR_COMMAND);
 }
 
-void LCD_ClearCells(u8 row, u8 col, u8 cell)
+void LCD_clearCells(u8 row, u8 col, u8 cell)
 {
-	LCD_GoTo(row, col);
+	LCD_goTo(row, col);
 	for (u8 i = 0; i < cell; i++)
 	{
-		LCD_WriteChar(' ');
+		LCD_writeChar(' ');
 	}
-	LCD_GoTo(row, col);
+	LCD_goTo(row, col);
 }
--- HAL/LCD.h
+++ HAL/LCD.h
@@ -1,5 +1,5 @@
 /**
- * @file LCD_Interface.h
+ * @file lcd.h
  * @author Ahmed Sabry (ahmed.sabry10696@gmail.com)
  * @brief LCD interface header file
  * @version 0.1
@@ -14,7 +14,7 @@
 
 #define F_CPU 8000000UL
 #include <util/delay.h>
-#include "DIO.h"
+#include "dio.h"
 
 /* LCD Data bits mode configuration: 8 or 4 */
 #define DATA_BITS_MODE 8
@@ -49,27 +49,27 @@
  * @brief initialize lcd 
  * 
  */
-extern void LCD_Init(void);
+extern void LCD_init(void);
 
 /**
  * @brief clear lcd display
  * 
  */
-extern void LCD_Clear(void);
+extern void LCD_clear(void);
 
 /**
  * @brief display char on lcd
  * 
  * @param data char to display
  */
-extern void LCD_WriteChar(const u8 ch);
+extern void LCD_writeChar(const u8 ch);
 
 /**
  * @brief display string on lcd
  * 
  * @param str string to display
  */
-extern void LCD_WriteString(const u8 *str);
+extern void LCD_writeString(const u8 *str);
 
 /**
  * @brief go to specific row and column in lcd display
@@ -77,7 +77,7 @@ extern void LCD_WriteString(const u8 *str);
  * @param row 
  * @param col 
  */
-extern void LCD_GoTo(u8 row, u8 col);
+extern void LCD_goTo(u8 row, u8 col);
 
 /**
  * @brief display string at specific row and column
@@ -86,14 +86,14 @@ extern void LCD_GoTo(u8 row, u8 col);
  * @param col col of beginning to display 
  * @param str string to display
  */
-extern void LCD_WriteStringRowCol(u8 row, u8 col, const u8 *str);
+extern void LCD_writeStringRowCol(u8 row, u8 col, const u8 *str);
 
 /**
  * @brief convert num to string and display it on lcd
  * 
  * @param data integer data to display on lcd
  */
-extern void LCD_WriteNum(s32 num);
+extern void LCD_writeNum(s32 num);
 
 /**
  * @brief create special char on lcd
@@ -101,28 +101,28 @@ extern void LCD_WriteNum(s32 num);
  * @param Pattern array contains the pattern of special char
  * @param Location location to store this pattern in lcd
  */
-extern void LCD_Create_Char(const u8 *Pattern, const u8 Location);
+extern void LCD_createChar(const u8 *Pattern, const u8 Location);
 
 /**
  * @brief display binary representation of num
  * 
  * @param num number to display
  */
-extern void LCD_WriteBinary(u16 num);
+extern void LCD_writeBinary(u16 num);
 
 /**
  * @brief write hex of value of the number
  * 
  * @param num decimal number
  */
-extern void LCD_WriteHex(u16 num);
+extern void LCD_writeHex(u16 num);
 
 /**
  * @brief display float numbers on lcd
  * 
  * @param num float number to display
  */
-extern void LCD_WriteFloat(f32 num);
+extern void LCD_writeFloat(f32 num);
 
 /**
  * @brief clear cells on lcd and return again to row and col
@@ -131,6 +131,6 @@ extern void LCD_WriteFloat(f32 num);
  * @param col col to start from
  * @param cell numbers of cells to clear
  */
-extern void LCD_ClearCells(u8 row, u8 col, u8 cell);
+extern void LCD_clearCells(u8 row, u8 col, u8 cell);
 
 #endif /* LCD_H_ */
