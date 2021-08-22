67195eb5ff96633f08f27c4bccf940acfde0ef44
AhmedSabry10696 <ahmed.sabry10696@gmail.com>
2021-08-23 00:01:13 +0200

    Atmel building process

--- Release/main.d
+++ Release/main.d
@@ -6,8 +6,7 @@ main.d main.o: .././main.c \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
  ../LIB/memory_map.h ../LIB/std_types.h ../LIB/utils.h ../LIB/std_types.h \
- ../MCAL/dio.h ../MCAL/ext_interrupt.h ../MCAL/uart.h ../HAL/lcd.h \
- ../HAL/EXT_EEPROM.h ../MCAL/i2c.h
+ ../MCAL/dio.h ../MCAL/ext_interrupt.h ../HAL/lcd.h ../MCAL/adc.h
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
 
@@ -33,10 +32,6 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\
 
 ../MCAL/ext_interrupt.h:
 
-../MCAL/uart.h:
-
 ../HAL/lcd.h:
 
-../HAL/EXT_EEPROM.h:
-
-../MCAL/i2c.h:
+../MCAL/adc.h:
