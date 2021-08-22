4c04ce6380087132b3e0df2030f056b1c13e850b
AhmedSabry10696 <ahmed.sabry10696@gmail.com>
2021-08-23 00:00:45 +0200

    app

--- main.c
+++ main.c
@@ -1,33 +1,28 @@
 #define  F_CPU 8000000UL
 #include <util/delay.h>
-
 #include "memory_map.h"
 #include "utils.h"
 #include "std_types.h"
 #include "dio.h"
 #include "ext_interrupt.h"
-#include "uart.h"
 #include "lcd.h"
-#include "EXT_EEPROM.h"
+#include "adc.h"
 
 int main(void)
 {
 	sei();
 	DIO_init();
 	LCD_init();	
-	EEPROM_init();
-	u8 y = 0;
-	
-	EEPROM_writeByte(512,10);
-	_delay_ms(10);
-	EEPROM_readByte(512,&y);
-		
-	LCD_goTo(2,0);
-	LCD_writeNum(y);
-	
+	ADC_init(REF_256V,ADC_SCALER_64);
+	u16 x,y;
 	while(1)
 	{	
-
+		
+		x = ADC_read(CH_0);
+		y = (u32)150*x/600;
+		LCD_goTo(0,0);
+		LCD_writeNum(y);
+		LCD_writeString("   ");
 	}
 
 	return 0;
