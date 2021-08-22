699bd8e7356305b96c210c140c4b949f3534c798
AhmedSabry10696 <ahmed.sabry10696@gmail.com>
2021-08-23 01:38:01 +0200

    ultra sonic driver

--- HAL/ultra_sonic.c
+++ HAL/ultra_sonic.c
@@ -14,6 +14,7 @@
 static volatile u16 t1, t2;
 static volatile u8 flag = 3;
 
+/* function to run when input capture unit interrupt fired */
 static void ICU_func(void)
 {
     if (0 == flag)
@@ -31,6 +32,7 @@ static void ICU_func(void)
 
 void UlTRASONIC_init(void)
 {
+    /* max 2^16 tick, tick time = 1 us */
     TIMER1_init(TIMER1_NORMAL_MODE, TIMER1_SCALER_8);
     TIMER1_ICU_intSetCallBack(ICU_func);
 }
@@ -47,10 +49,11 @@ u16 ULTRASONIC_Read(Ultrasonic_type ultra)
     _delay_us(10);
     DIO_writePin(ultra, LOW);
 
-    while (flag < 2)
-        ;
+    while (flag < 2);
+
     time = t2 - t1;
     d = time / 58;
+    
     return d;
 }
 
