c42b42dfb47d9b218c8e295ca47cced1a30b86a6
AhmedSabry10696 <ahmed.sabry10696@gmail.com>
2021-08-22 23:55:22 +0200

    dio config file

--- MCAL/dio_config.c
+++ MCAL/dio_config.c
@@ -13,16 +13,16 @@
 
 const DioPinStatus_type pins_status[TOTAL_PINS] = 
 {
-    /****** PORTA PINS 0:7 ******/
-    OUTPUT,     /* ADC0         */
-    OUTPUT,     /* ADC1         */
-    OUTPUT,     /* ADC2         */
-    OUTPUT,     /* ADC3         */
-    OUTPUT,     /* ADC4         */
-    OUTPUT,     /* ADC5         */
-    OUTPUT,     /* ADC6         */
-    OUTPUT,     /* ADC7         */
-    /****************************/
+    /*********************** PORTA PINS 0:7 *************************/
+    OUTPUT,     /* ADC0                                             */
+    OUTPUT,     /* ADC1                                             */
+    OUTPUT,     /* ADC2                                             */
+    OUTPUT,     /* ADC3                                             */
+    OUTPUT,     /* ADC4                                             */
+    OUTPUT,     /* ADC5                                             */
+    OUTPUT,     /* ADC6                                             */
+    OUTPUT,     /* ADC7                                             */
+    /****************************************************************/
 
     /********************** PORTB PINS 0:7 **************************/
     OUTPUT,     /* XCK/T0 {usart clock}/{T0 pin ext clock/counter}  */
@@ -35,25 +35,25 @@ const DioPinStatus_type pins_status[TOTAL_PINS] =
     OUTPUT,     /* SCK    {SPI OUTPUT(master),INFREE(slave)}        */
     /****************************************************************/
 
-    /********** PORTC PINS 0:7 **********/    
-    INFREE,     /* SCL {I2C INFREE}     */
-    INFREE,     /* SDA {I2C INFREE}     */
-    OUTPUT,     /* TCK                  */
-    OUTPUT,     /* TMS                  */
-    OUTPUT,     /* TDO                  */
-    OUTPUT,     /* TDI                  */
-    OUTPUT,     /* TOSC1  {T2}          */
-    OUTPUT,     /* TOSC2  {T2}          */
-    /************************************/
+    /************************ PORTC PINS 0:7 ************************/    
+    INFREE,     /* SCL {I2C INFREE}                                 */
+    INFREE,     /* SDA {I2C INFREE}                                 */
+    OUTPUT,     /* TCK                                              */
+    OUTPUT,     /* TMS                                              */  
+    OUTPUT,     /* TDO                                              */
+    OUTPUT,     /* TDI                                              */
+    OUTPUT,     /* TOSC1  {T2}                                      */
+    OUTPUT,     /* TOSC2  {T2}                                      */
+    /****************************************************************/
 
-    /*************** PORTD PINS 0:7 *************/
-    OUTPUT,     /* RXD  {uart INFREE}           */
-    OUTPUT,     /* TXD  {uart OUTPUT}           */
-    OUTPUT,     /* INT0 {ext interrupt 0}       */
-    OUTPUT,     /* INT1 {ext interrupt 1}       */
-    OUTPUT,     /* OC1B {output compare B T1}   */ 
-    OUTPUT,     /* OC1A {output compare A T1}   */
-    OUTPUT,     /* ICP1 {input capture pin T1}  */
-    OUTPUT      /* OC2  {output compare pin T2} */
-    /********************************************/
+    /************************ PORTD PINS 0:7 ************************/
+    OUTPUT,     /* RXD  {uart INFREE}                               */
+    OUTPUT,     /* TXD  {uart OUTPUT}                               */
+    OUTPUT,     /* INT0 {ext interrupt 0}                           */
+    OUTPUT,     /* INT1 {ext interrupt 1}                           */
+    OUTPUT,     /* OC1B {output compare B T1}                       */ 
+    OUTPUT,     /* OC1A {output compare A T1}                       */
+    OUTPUT,     /* ICP1 {input capture pin T1}                      */
+    OUTPUT      /* OC2  {output compare pin T2}                     */
+    /****************************************************************/
 };
