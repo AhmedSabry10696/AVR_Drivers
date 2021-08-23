43371f6392956d18098e10573694b52bc8ed5a06
AhmedSabry10696 <ahmed.sabry10696@gmail.com>
2021-08-24 01:45:28 +0200

    uart config file

--- MCAL/uart_config.c
+++ MCAL/uart_config.c
@@ -12,20 +12,20 @@
 #include "uart.h"
 
 /***********************************************************
- * @brief   Configuration struct for Uart with FCPU= 8 MHZ
+ * @brief   Configuration struct for Uart with FCPU = 8 MHZ
  ***********************************************************
- * @parity:  NO_PARITY,EVEN_PARITY,ODD_PARITY 
+ * @parity: NO_PARITY  EVEN_PARITY  ODD_PARITY 
  ***********************************************************
- * @stop:    ONE_STOP_BIT,TWO_STOP_BIT
+ * @stop:   ONE_STOP_BIT   TWO_STOP_BIT
  ***********************************************************
- * @data:    FIVE_BIT_DATA,SIX_BIT_DATA,SEVEN_BIT_DATA,
- *          EIGHT_BIT_DATA,NINE_BIT_DATA
+ * @data:   FIVE_BIT_DATA  SIX_BIT_DATA    SEVEN_BIT_DATA
+ *          EIGHT_BIT_DATA  NINE_BIT_DATA
  ***********************************************************
- * @baud:    BAUD_2400,BAUD_4800,BAUD_9600,
- *          BAUD_14400,BAUD_19200,BAUD_28800,
- *          BAUD_38400,BAUD_75600,BAUD_76800,
- *          BAUD_115200,BAUD_230400,BAUD_250000,
- *          BAUD_500000,BAUD_1000000
+ * @baud:   BAUD_2400       BAUD_4800       BAUD_9600,
+ *          BAUD_14400      BAUD_19200      BAUD_28800,
+ *          BAUD_38400      BAUD_75600      BAUD_76800,
+ *          BAUD_115200     BAUD_230400     BAUD_250000,
+ *          BAUD_500000     BAUD_1000000
  ***********************************************************/
 
 const UartConfig uart =
